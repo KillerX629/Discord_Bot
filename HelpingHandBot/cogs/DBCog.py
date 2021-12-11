05536852fa471dd9f6d5692387b4fbff7bf92b36
Ignacio Pita <napita2007@gmail.com>
2021-12-11 02:09:47 -0300

    funcion crearTrabajo

--- HelpingHandBot/cogs/DBCog.py
+++ HelpingHandBot/cogs/DBCog.py
@@ -3,17 +3,12 @@ import pymongo
 import discord
 from discord.ext import commands
 from discord import option
-import os
 import sys
 
-current = os.path.dirname(os.path.realpath(__file__))
-
-parent = os.path.dirname(current)
-
-sys.path.append(parent)
-
+sys.path.append('HelpingHandBot')
 
 from auxFunctions import *
+
 
 
 testServers = read_guilds()
@@ -36,7 +31,7 @@ class DBCog(commands.Cog):
     async def testaddmoney(self, ctx, 
                            usuario:Option(discord.Member,'El usuario a quien se le añadirá'),
                            cantidad:Option(int,'La cantidad de dinero a añadir')):
-        print(read_access())
+        
         
         if self.cluster.get_database(name=str(ctx.guild.id)).get_collection("users").find_one({"_id":usuario.id}) is None:
             self.cluster.get_database(name=str(ctx.guild.id)).get_collection(
@@ -44,4 +39,19 @@ class DBCog(commands.Cog):
         else:
             self.cluster.get_database(name=str(ctx.guild.id)).get_collection(
                 "users").update_one({"_id": usuario.id}, {"$inc": {"money": cantidad}})
+            
+        await ctx.respond("Se ha añadido " + str(cantidad) + " monedas a " + usuario.name)
 
+    @commands.slash_command(guild_ids=testServers, name="createWork")
+    async def createWork(self, ctx,
+                         nombre:Option(str,'El nombre del trabajo'),
+                         paga:Option(int,'La paga del trabajo'),
+                         descripcion:Option(str,'La descripción del trabajo'),
+                         tiempo:Option(int,'El tiempo que tarda en completarse el trabajo en minutos'),
+                         produce:Option(str,'El producto que produce el trabajo')):
+        
+        if self.cluster.get_database(name=str(ctx.guild.id)).get_collection("works").find_one({"_id":nombre}) is None:#falta checkear si el item que produce existe
+            self.cluster.get_database(name=str(ctx.guild.id)).get_collection(
+                "works").insert_one({"_id": nombre, "paga": paga, "descripcion": descripcion, "tiempo": tiempo, "produce": produce})
+        else:
+            await ctx.respond("Ya existe un trabajo con ese nombre")
